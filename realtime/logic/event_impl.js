f063284ab87bfcb349a1a009a1bae5239ea7d0cb
ahmed-debbech <debbech.ahmed@gmail.com>
2024-08-25 16:46:28 +0100

    add fix in real time

--- realtime/logic/event_impl.js
+++ realtime/logic/event_impl.js
@@ -21,10 +21,11 @@ async function onConnected(user){
     user.name = naming;
     if(naming != '-'){
         connected_users.push(user)
+        let greeting = chooseGreetingMsg()
         for(let i =0; i<=connected_users.length-1; i++){
             if(connected_users[i].ws.readyState == WebSocket.OPEN){
                 if(connected_users[i].id != user.id){
-                    let msg = new Message("SYSTEM" , JSON.stringify({greet: user.name + chooseGreetingMsg()}), Date.now());
+                    let msg = new Message("SYSTEM" , JSON.stringify({greet: user.name + greeting}), Date.now());
                     connected_users[i].ws.send(JSON.stringify(msg).toString());
                 }else{
                     let msg = new Message("SYSTEM" , JSON.stringify({id: user.id, name: user.name}), Date.now());
