cb36e30bfed946e64e15b875698b3da4b0bb708c
ahmed-debbech <debbech.ahmed@gmail.com>
2024-08-31 16:53:29 +0100

    fix cooldown timming

--- realtime/network/namer.js
+++ realtime/network/namer.js
@@ -2,7 +2,7 @@ var axios = require('axios')
 
 async function call_namer(){
   try{
-    var name = await axios.get('http://197.17.11.79:6062/gen',{ timeout: 7000})
+    var name = await axios.get('http://192.168.1.9:6062/gen',{ timeout: 7000})
   }catch(e){
     console.log('failed to connect to microservice namer')
     return '-'
