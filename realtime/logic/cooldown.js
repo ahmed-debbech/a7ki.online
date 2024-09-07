3ed65b670420e6b25b2417929f81adb9fe098099
ahmed-debbech <debbech.ahmed@gmail.com>
2024-09-07 12:06:53 +0100

    implement cooldown in backend

--- realtime/logic/cooldown.js
+++ realtime/logic/cooldown.js
@@ -2,8 +2,14 @@ var cooldown_pool = {}
 
 function checkCooldown(user_id){
     if(cooldown_pool[user_id]){
-        
+        let timest = cooldown_pool[user_id]
+        const ellapsedMilli = Date.now() - timest
+        if(Math.floor(ellapsedMilli / 1000) < 4){
+            console.log(ellapsedMilli)
+            return false
+        }
     }
+    cooldown_pool[user_id] = Date.now()
     return true;
 }
 
