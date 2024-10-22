e2bb3e1a911c42f97c6a8afc5f8328b7b316288d
ahmed-debbech <debbech.ahmed@gmail.com>
2024-10-22 20:07:55 +0100

    decompose sidebar and navbar into two components

--- dash/src/app/app.module.ts
+++ dash/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
