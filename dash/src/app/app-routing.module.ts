679b7faf2acc3c7a47a1affb30e1686065b9bf5c
ahmed-debbech <debbech.ahmed@gmail.com>
2024-10-22 23:45:58 +0100

    add users section and routing

--- dash/src/app/app-routing.module.ts
+++ dash/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashComponent } from './components/dash/dash.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashComponent },
+  { path: 'users', component: UsersComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dash/src/app/app.module.ts
+++ dash/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { DashComponent } from './components/dash/dash.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    DashComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
